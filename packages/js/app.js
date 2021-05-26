8d8caf99d5d36d6ea31216bc599f8bebc6bd3ea6
Ollaster <68718143+Ollaster@users.noreply.github.com>
2021-05-26 09:20:53 -0300

    Add files via upload

---
+++ packages/js/app.js
@@ -0,0 +1,12 @@
+const price = document.getElementById('price');
+const amount = document.getElementById('amount');
+const calc = document.getElementById('calc');
+
+
+calc.addEventListener("click", () => {
+
+});
+
+
+
+
